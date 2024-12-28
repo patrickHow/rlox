40a253970bb2b1f1deb2aa9da0714d9a89f21fcf
Patrick Howell <hellopatrickhowell@gmail.com>
2024-12-28 12:59:56 -0500

    Add binary op instructions

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,25 @@
 
 const DEFAULT_CHUNK_LEN: usize = 16;
 
-type ConstantType = f64; 
+// The core "value" type that operations work on
+type Value = f64; 
 
 // OPCODES - defining as u8 instead of enum so we can do direct comparisons
 // without all kinds of nasty casting 
 
 const OP_CONSTANT: u8 = 0; // [opcode][constant index] - pushes a constant to the stack
 const OP_RETURN: u8 = 1; // [opcode] - pops a constant from the stack and prints it
-const OP_NEGATE: u8 = 2; // [opcode] - negates the value at the top of the stack
+const OP_ADD: u8 = 2; // [opcode] - adds the last two values on the stack
+const OP_SUBTRACT: u8 = 3; // [opcode] - subtracts the last value on the stack from the second-from-last value on the stack
+const OP_MULTIPLY: u8 = 4; // [opcode] - multiplies the last two values on the stack
+const OP_DIVIDE: u8 = 5; // [opcode] - divides the last value on the stack by the second-from-last value on the stack
+const OP_NEGATE: u8 = 6; // [opcode] - negates the value at the top of the stack
 
-
+// Const functions for doing binary operations
+const fn add(a: f64, b: f64) -> f64 {a + b}
+const fn sub(a: f64, b: f64) -> f64 {a - b}
+const fn mul(a: f64, b: f64) -> f64 {a * b}
+const fn div(a: f64, b: f64) -> f64 {a / b}
 
 fn simple_instruction(name: String, offset: usize) -> usize {
     println!("{name}");
@@ -23,7 +32,7 @@ fn simple_instruction(name: String, offset: usize) -> usize {
 struct Chunk {
     code: Vec<u8>,
     lines: Vec<u32>,
-    constants: Vec<ConstantType>,
+    constants: Vec<Value>,
 }
 
 impl Chunk {
@@ -66,6 +75,11 @@ impl Chunk {
         match instruction {
             OP_RETURN => simple_instruction("OP_RETURN".to_string(), offset),
             OP_CONSTANT => self.constant_instruction("OP_CONSTANT".to_string(), offset),
+            OP_ADD => simple_instruction("OP_ADD".to_string(), offset),
+            OP_SUBTRACT => simple_instruction("OP_SUBTRACT".to_string(), offset),
+            OP_MULTIPLY => simple_instruction("OP_MULTIPLY".to_string(), offset),
+            OP_DIVIDE => simple_instruction("OP_DIVIDE".to_string(), offset),
+
             OP_NEGATE => simple_instruction("OP_NEGATE".to_string(), offset),
             _ => {
                 println!("Invalid opcode {instruction}");
@@ -93,7 +107,7 @@ impl Chunk {
     // Write a constant value to the chunk
     // Returns the index of the added value for later use
     // TODO fix if we need more than 256 constants per chunk (but...why)
-    fn add_constant(&mut self, value: ConstantType) -> u8 {
+    fn add_constant(&mut self, value: Value) -> u8 {
         self.constants.push(value);
         return (self.constants.len() - 1) as u8
     }
@@ -102,7 +116,7 @@ impl Chunk {
 struct VM {
     chunk: Chunk, // The current chunk being executed
     ip: usize, // Instruction pointer - an index into the code array in the vector
-    stack: Vec<ConstantType>, // A stack for values in the VM 
+    stack: Vec<Value>, // A stack for values in the VM 
 }
 
 enum InterpretResult {
@@ -122,6 +136,12 @@ impl VM {
 
     fn reset_stack(&mut self) {
         self.stack.clear();
+    }
+
+    fn binary_op(&mut self, op: fn(f64, f64) -> f64) {
+        let b = self.stack.pop().unwrap();
+        let a = self.stack.pop().unwrap();
+        self.stack.push(op(a, b));
     }
 
     fn interpret(&mut self, chunk: Chunk) -> InterpretResult {
@@ -157,6 +177,10 @@ impl VM {
                     println!("{val}");
                     return InterpretResult::OK
                 }
+                OP_ADD => self.binary_op(add),
+                OP_SUBTRACT => self.binary_op(sub),
+                OP_MULTIPLY => self.binary_op(mul),
+                OP_DIVIDE => self.binary_op(div),
                 OP_NEGATE => {
                     let top_idx = self.stack.len() - 1;
                     self.stack[top_idx] = -self.stack[top_idx];
@@ -176,6 +200,19 @@ fn main() {
     let ind = chunk.add_constant(1.2);
     chunk.write(OP_CONSTANT, 123);
     chunk.write(ind, 123);
+    
+    let ind = chunk.add_constant(3.4);
+    chunk.write(OP_CONSTANT, 123);
+    chunk.write(ind, 123);
+
+    chunk.write(OP_ADD, 123);
+
+    let ind = chunk.add_constant(5.6);
+    chunk.write(OP_CONSTANT, 123);
+    chunk.write(ind, 123);
+
+    chunk.write(OP_DIVIDE, 123);
+
     chunk.write(OP_NEGATE, 123);
     chunk.write(OP_RETURN, 123);
 
