f5e6131a436ca7b4ffabe77d670aa31f450633c9
Patrick Howell <hellopatrickhowell@gmail.com>
2025-01-01 12:10:37 -0500

    Fleshing out compiler

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,4 +1,5 @@
 use crate::chunk::Chunk;
+use crate::opcodes;
 use crate::scanner::{ScanError, Scanner};
 use crate::token::{TokenType, Token};
 
@@ -82,7 +83,7 @@ impl Parser {
 }
 
 pub struct Compiler{
-    
+
 }
 
 impl Compiler {
@@ -98,6 +99,27 @@ impl Compiler {
         parser.consume(TokenType::Eof, "Expected end of expression".to_string());
         
 
-        return Some(chunk);
+
+        
+        if parser.has_error {
+            return None
+        } else {
+            return Some(chunk)
+        }
+    }
+
+    fn emit_byte(&self, byte: u8, chunk: &mut Chunk, line: u32) {
+        chunk.write(byte, line);
+    }
+
+    // Utility function for the common use case of writing an instruction
+    // followed by a 1-byte operand
+    fn emit_bytes(&self, byte1: u8, byte2: u8, chunk: &mut Chunk, line: u32) {
+        chunk.write(byte1, line);
+        chunk.write(byte2, line);
+    }
+
+    fn end(&self, chunk: &mut Chunk, line: u32) {
+        self.emit_byte(opcodes::OP_RETURN, chunk, line);
     }
 }
